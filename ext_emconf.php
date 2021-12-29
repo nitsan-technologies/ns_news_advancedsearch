58542fa4aab92b17951552364771bea1e73bded8
Divya <gdivya.nitsan@gmail.com>
2021-12-29 18:59:03 +0530

    [TODO] - v11 Compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,11 +12,11 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.2.2',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.2.0-10.4.99',
-            'news' => '3.0.0-8.9.9',
+            'typo3' => '6.2.0-11.5.99',
+            'news' => '3.0.0-9.1.1',
         ],
         'conflicts' => [],
         'suggests' => [],
