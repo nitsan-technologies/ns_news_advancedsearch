d889bb72f4099362fc79da9d79724dc8b5dda207
NITSAN Technologies <sanjay@nitsan.in>
2021-12-31 17:27:44 +0530

    [TASK] Final review and ready to release v2

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,8 +1,8 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['ns_news_advancedsearch'] = [
     'title' => '[NITSAN] Advanced Search for EXT:news',
-    'description' => 'Do you want more rich search features into your favourite EXT:news? By installing this extension, you can search by category, title, teaser etc. Know  more in manual. Live-Demo: https://demo.t3terminal.com/t3t-extensions/news-comments-1/news-advance-search/ You can download PRO version for more-features & free-support at https://t3terminal.com/advanced-search-for-typo3-news-extension/',
+    'description' => 'Do you want more rich search features into your favourite EXT:news? By installing this extension, you can search by category, title, teaser etc. Know  more in manual. Live-Demo: https://demo.t3terminal.com//t3t-extensions/news-advancedsearch You can download PRO version for more-features & free-support at https://t3terminal.com/advanced-search-typo3-news-extension',
     'category' => 'plugin',
     'author' => 'NITSAN Technologies Pvt Ltd',
     'author_email' => 'sanjay@nitsan.in',
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '6.2.0-11.5.99',
-            'news' => '3.0.0-9.1.1',
+            'news' => '3.0.0-9.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
