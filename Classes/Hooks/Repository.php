c387d5b3882c37cb147f7b69681e15af875e735e
Khushi Rupera <khushirupera.nitsan@gmail.com>
2024-10-25 16:43:28 +0530

    [BUGFIX]Fixed issue in searching functionality

--- Classes/Hooks/Repository.php
+++ Classes/Hooks/Repository.php
@@ -4,8 +4,7 @@ namespace NITSAN\NsNewsAdvancedsearch\Hooks;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Persistence\QueryInterface;
-use TYPO3\CMS\Core\Http\ServerRequest;
-use TYPO3\CMS\Core\Utility\Typo3Mode;
+use Psr\Http\Message\ServerRequestInterface;
 
 class Repository
 {
@@ -20,25 +19,21 @@ class Repository
      */
     protected function updateConstraints(QueryInterface $query, array &$constraints)
     {
-        // Get the current request
-        $request = GeneralUtility::makeInstance(ServerRequest::class);
-       
-
-        // Extract 'search' parameters from the GET request
+        $request = $GLOBALS['TYPO3_REQUEST'];
         $actionRequest = $request->getQueryParams()['tx_news_pi1']['search'] ?? null;
-       
-        if(isset($actionRequest)) {
+        
+        if (isset($actionRequest)) {
             $actionRequest['category'] = $actionRequest['category'] ?? '0';
             $actionRequest['teaser'] = $actionRequest['teaser'] ?? '';
             $actionRequest['title'] = $actionRequest['title'] ?? '';
-            
-            if($actionRequest['category'] || $actionRequest['teaser'] || $actionRequest['title']) {
+
+            if ($actionRequest['category'] || $actionRequest['teaser'] || $actionRequest['title']) {
                 // Filter Categories
-                if($actionRequest['category']) {
+                if ($actionRequest['category']) {
                     $constCategory = [];
                     $searchCategories = $actionRequest['category'];
                     foreach ($searchCategories as $categories) {
-                        if($categories == '0') {
+                        if ($categories == '0') {
                             $constCategory[] = $query->greaterThan('categories', 0);
                         } else {
                             $constCategory[] = $query->contains('categories', $categories);
@@ -46,18 +41,16 @@ class Repository
                     }
                     $constraints[] = $query->logicalOr(...array_values($constCategory));
                 }
-                
+
                 // Filter Teaser Text
-                if($actionRequest['teaser']) {
+                if ($actionRequest['teaser']) {
                     $constraints[] = $query->like('teaser', '%' . $actionRequest['teaser'] . '%');
                 }
 
                 // Filter Title Text
-                if($actionRequest['title']) {
-                  $result =  $constraints[] = $query->like('title', '%' . $actionRequest['title'] . '%');
-                  \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($result, __FILE__.' Line '.__LINE__);die;
+                if ($actionRequest['title']) {
+                    $constraints[] = $query->like('title', '%' . $actionRequest['title'] . '%');
                 }
-                
             }
         }
     }
